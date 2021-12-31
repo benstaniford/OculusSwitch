55131d521587437b68d50a8a19caac92fc27f141
Ben Staniford <bstaniford@beyondtrust.com>
2021-12-31 18:45:53 +0000

    COM registration stuff implemented

--- OculusSwitch/OculusSwitchService/OculusControlComService.cs
+++ OculusSwitch/OculusSwitchService/OculusControlComService.cs
@@ -1,10 +1,35 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace OculusSwitchService
 {
-    [ComVisible(true)]
-    public class OculusControlComService
+    [Guid("83C40736-3189-44bc-AB0F-9FB3703EA172"), ComVisible(true)]  //IID_IOculusControlComService
+    public interface IOculusControlComService
     {
+        int TestingThings();
+    }
+
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("F47CC890-2151-4DC9-AD0B-AB970EB14466"), ComVisible(true)]
+    public class OculusControlComService : IOculusControlComService
+    {
+        public static Guid ClassId = new Guid("F47CC890-2151-4DC9-AD0B-AB970EB14466");
+
+        public OculusControlComService()
+        {
+            // Initialize COM security  (https://www.pinvoke.net/default.aspx/ole32.coinitializesecurity suggests this won't work due to CoInitialize being called to early)
+            /*
+            int hResult = COMNative.CoInitializeSecurity(
+                IntPtr.Zero,    // Add your security descriptor here
+                -1, IntPtr.Zero, IntPtr.Zero, RPC_C_AUTHN_LEVEL.PKT_PRIVACY,
+                RPC_C_IMP_LEVEL.IDENTIFY, IntPtr.Zero,
+                EOLE_AUTHENTICATION_CAPABILITIES.DISABLE_AAA |
+                EOLE_AUTHENTICATION_CAPABILITIES.SECURE_REFS |
+                EOLE_AUTHENTICATION_CAPABILITIES.NO_CUSTOM_MARSHAL,
+               IntPtr.Zero);
+            */
+        }
+
         public int TestingThings()
         {
             return 23;
--- OculusSwitch/OculusSwitchService/OculusSwitchService.cs
+++ OculusSwitch/OculusSwitchService/OculusSwitchService.cs
@@ -4,6 +4,8 @@ namespace OculusSwitchService
 {
     public partial class OculusSwitchService : ServiceBase
     {
+        uint _cookie = 0;
+
         public OculusSwitchService()
         {
             InitializeComponent();
@@ -11,10 +13,18 @@ namespace OculusSwitchService
 
         protected override void OnStart(string[] args)
         {
+            // Register the SimpleObject class object on start
+            int hResult = COMNative.CoRegisterClassObject(
+                OculusControlComService.ClassId,              // CLSID to be registered
+                new OculusControlComServiceClassFactory(),    // Class factory
+                (uint)COMNative.CLSCTX.CLSCTX_LOCAL_SERVER,   // Context to run
+                (uint)COMNative.REGCLS.MULTIPLEUSE,
+                out _cookie);
         }
 
         protected override void OnStop()
         {
+            COMNative.CoRevokeClassObject(_cookie);
         }
     }
 }
